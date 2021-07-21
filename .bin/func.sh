305a9be1b2cc52b1ff18ccd5f69575a11d2ca2a5
Penkwe Tjengh <498296816a@gmail.com>
2021-07-21 10:09:59 +0800

    add .bin

---
+++ .bin/func.sh
@@ -0,0 +1,18 @@
+function set-clash() {
+	ln -s /home/linuxbrew/.linuxbrew/opt/clash/bin/clash /usr/local/bin/clash
+	curl https://efanyun.me/clash/207089/uWJGgmHgzX/ > /etc/clash/config.yaml
+	cp /etc/clash/config.yaml /home/frind/.config/clash/config.yaml
+}
+
+function set-swap() {
+	swapoff -a
+	dd if=/dev/zero of=/swapfile bs=1G count=$1
+	chmod 600 /swapfile
+	mkswap /swapfile
+	swapon /swapfile
+}
+
+function cmd() {
+	mkdir $1
+	cd $1
+}
