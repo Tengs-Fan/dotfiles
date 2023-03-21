a919cc1d27d7fc223a63d4598b7e2ebff4e1d8bc
Tengs Penkwe <tengspenkwe@gmail.com>
2023-03-20 18:12:42 -0700

    chatGPT in terminal

---
+++ .bin/chat.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import openai
+
+openai.api_key = "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"
+
+message=[
+        {"role": "system", "content": "As a proficient programmer, you always think step by step"},
+        ]
+
+def append_new_message(info):
+    new =  {"role": "user", "content": info}
+    message.append(new)
+    # return message
+
+# Define a function to generate a chat response
+def generate_response():
+    # Generate chat response using OpenAI API
+    response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            # max_tokens=32768,
+            temperature=1.2,
+            messages = message
+            )
+    chat = response["choices"][0]["message"]["content"]
+    assist = {"role": "assistant", "content": chat}
+    message.append(assist)
+    # Return chat response
+    return chat
+
+# Start the chat loop
+print("Hello! I'm ChatGPT. How can I help you today?")
+while True:
+    # Get user input
+    user_input = input("You: ")
+    append_new_message(user_input)
+    # Generate chat response
+    chat_response = generate_response()
+    # Print chat response
+    print(chat_response)
