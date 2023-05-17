752a7883e12de0557c8cf8bc7600c83a3ec1739d
Tengs Penkwe <tengspenkwe@gmail.com>
2023-05-17 15:57:44 -0700

    function set-conda()

--- .bin/env.sh
+++ .bin/env.sh
@@ -25,6 +25,23 @@ function set-brew(){
 	export CPPFLAGS="-I$HOMEBREW_ROOT/include $CPPFLAGS"
 }
 
+function set-conda(){
+  # >>> conda initialize >>>
+  # !! Contents within this block are managed by 'conda init' !!
+  __conda_setup="$('$USER_ROOT/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+  if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+  else
+    if [ -f "$USER_ROOT/miniconda3/etc/profile.d/conda.sh" ]; then
+      . "$USER_ROOT/miniconda3/etc/profile.d/conda.sh"
+    else
+      export PATH="$USER_ROOT/miniconda3/bin:$PATH"
+    fi
+  fi
+  unset __conda_setup
+  # <<< conda initialize <<<
+}
+
 function set-rust(){
 	source "$HOME/.cargo/env"
 }
